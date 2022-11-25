e568b96807fc01ad7449e57559e94abd3fddb61d
ruthyi <jbruthlizcano@gmail.com>
2022-11-25 09:30:14 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//Cambio de color del body
+let i = 0;
+function changeColor(color) {
+    document.body.style.background = color;
+}
+function cambiar() {
+    if (i == 0) {
+        document.getElementById("cabecera").innerHTML = "Dark";
+        document.getElementById("general").style.background='lightgray';
+        document.getElementById("cal").style.color='black';   
+        i++;
+        changeColor('black');
+    } else {
+        document.getElementById("cabecera").innerHTML = "Light"; 
+        document.getElementById("cal").style.color='lightgray';       
+        document.getElementById("general").style.background='midnightblue';
+
+        i = 0;
+        changeColor('aliceblue');
+    }
+}
