d0b7fff4ff141fdd76b67f03cdb4ac2b886cbc72
ruthyi <jbruthlizcano@gmail.com>
2022-11-27 19:07:21 -0500

    calculator functionalities

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
 //Cambio de color del body
-let i = 0;
+let o = 0;
 function changeColor(color) {
     document.body.style.background = color;
 }
 function cambiar() {
-    if (i == 0) {
+    if (o == 0) {
         document.getElementById("cabecera").innerHTML = "Dark";
         document.getElementById("general").style.background='lightgray';
         document.getElementById("cal").style.color='black';   
-        i++;
+        o++;
         changeColor('black');
     } else {
         document.getElementById("cabecera").innerHTML = "Light"; 
         document.getElementById("cal").style.color='lightgray';       
         document.getElementById("general").style.background='midnightblue';
 
-        i = 0;
+        o = 0;
         changeColor('aliceblue');
     }
 }
-// Funcionalidad Calculadora
-//Variables
-let operandoa;
-let operandob;
-let operacion;
-
-//
